d0fa661bcced180a336f2584f14fbeabf7ea490f
Carson McManus <carson.mcmanus1@gmail.com>
2021-03-11 10:53:54 -0500

    add hw6

---
+++ hw6/max_mcmanus.py
@@ -0,0 +1,2 @@
+def max_name_len(firstname, lastname):
+	print(f"The largest value of the length of my firstname and  lastname is {max(len(firstname), len(lastname))}")
---
+++ hw6/prod_mcmanus.py
@@ -0,0 +1,2 @@
+def prod_len_name(firstname, lastname):
+	print(f"The product of the value of the lengths of my firstname and lastname is {len(firstname) * len(lastname)}")
